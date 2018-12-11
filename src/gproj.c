8ea92b212998300cf2954eec920b68c7c2d9df98
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-12-11 14:52:19 -0200

    rev

--- src/gproj.c
+++ src/gproj.c
@@ -69,28 +69,28 @@ int gproj(void)
 		if (input_buttons_states&INPUT_BUTTON_UP) {
 			player_spr.ss.pos.y = 0;
 			if ((SDL_GetTicks() - anim_clk) > 250) {
-				player_spr.ss.pos.x = (player_spr.ss.pos.x + 16) % 64;
+				player_spr.ss.pos.x = (player_spr.ss.pos.x + 16) % 32;
 				anim_clk = SDL_GetTicks();
 			}
 			player_spr.scr.pos.y -= 1;
 		} else if (input_buttons_states&INPUT_BUTTON_DOWN) {
 			player_spr.ss.pos.y = 32;
 			if ((SDL_GetTicks() - anim_clk) > 250) {
-				player_spr.ss.pos.x = (player_spr.ss.pos.x + 16) % 64;
+				player_spr.ss.pos.x = (player_spr.ss.pos.x + 16) % 32;
 				anim_clk = SDL_GetTicks();
 			}
 			player_spr.scr.pos.y += 1;
 		} else if (input_buttons_states&INPUT_BUTTON_LEFT) {
 			player_spr.ss.pos.y = 48;
 			if ((SDL_GetTicks() - anim_clk) > 250) {
-				player_spr.ss.pos.x = (player_spr.ss.pos.x + 16) % 64;
+				player_spr.ss.pos.x = (player_spr.ss.pos.x + 16) % 32;
 				anim_clk = SDL_GetTicks();
 			}
 			player_spr.scr.pos.x -= 1;
 		} else if (input_buttons_states&INPUT_BUTTON_RIGHT) {
 			player_spr.ss.pos.y = 16;
 			if ((SDL_GetTicks() - anim_clk) > 250) {
-				player_spr.ss.pos.x = (player_spr.ss.pos.x + 16) % 64;
+				player_spr.ss.pos.x = (player_spr.ss.pos.x + 16) % 32;
 				anim_clk = SDL_GetTicks();
 			}
 			player_spr.scr.pos.x += 1;
