2bb7005443d2b9020b2aff2628fb084e48c4fb1c
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-12-14 22:48:09 -0200

    attack test

--- src/player.c
+++ src/player.c
@@ -7,21 +7,27 @@
 input_button_t input_buttons_states;
 
 
-static const struct actor_frame walkup[] = {
+static const struct actor_frame walk_up[] = {
 	{ 150, { .size = { 16, 16 }, .pos = { 511, 3040 } } },
 	{ 150, { .size = { 16, 16 }, .pos = { 526, 3040 } } }
 };
-static const struct actor_frame walkdown[] = {
+static const struct actor_frame walk_down[] = {
 	{ 150, { .size = { 16, 16 }, .pos = { 512, 3072 } } },
 	{ 150, { .size = { 16, 16 }, .pos = { 527, 3072 } } }
 };
-static const struct actor_frame walkleft[] = {
+static const struct actor_frame walk_left[] = {
 	{ 150, { .size = { 16, 16 }, .pos = { 510, 3088 } } },
 	{ 150, { .size = { 16, 16 }, .pos = { 527, 3088 } } }
 };
-static const struct actor_frame walkright[] = {
+static const struct actor_frame walk_right[] = {
 	{ 150, { .size = { 16, 16 }, .pos = { 512, 3056 } } },
 	{ 150, { .size = { 16, 16 }, .pos = { 527, 3056 } } }
+};
+static const struct actor_frame attack_right[] = {
+	{ 150, { .size = { 16, 16 }, .pos = { 544, 3056 } } },
+	{ 125, { .size = { 16, 16 }, .pos = { 559, 3056 } } },
+	{ 100, { .size = { 16, 16 }, .pos = { 575, 3056 } } },
+	{ 75, { .size = { 16, 16 }, .pos = { 591, 3056 } } }
 };
 
 static struct actor actor = {
@@ -32,7 +38,7 @@ static struct actor actor = {
 static int frame_idx = 0;
 static uint32_t frame_clk = 0;
 static uint32_t frame_duration = 0;
-
+static int is_attacking = 0;
 static float velocity = 2.5;
 
 
@@ -45,30 +51,43 @@ void player_init(void)
 
 void player_update(void)
 {
-	if (input_buttons_states == 0)
-		return;
-
 
 	const uint32_t now = timer_now();
 	const struct actor_frame* nextf = NULL;
 	int arrsz = 0;
 
-	if (input_buttons_states&INPUT_BUTTON_UP) {
-		actor.scr.pos.y -= velocity * 1;
-		nextf = walkup;
-		arrsz = ARRSZ(walkup);
-	} else if (input_buttons_states&INPUT_BUTTON_DOWN) {
-		actor.scr.pos.y += velocity * 1;
-		nextf = walkdown;
-		arrsz = ARRSZ(walkdown);
-	} else if (input_buttons_states&INPUT_BUTTON_LEFT) {
-		actor.scr.pos.x -= velocity * 1;
-		nextf = walkleft;
-		arrsz = ARRSZ(walkleft);
-	} else if (input_buttons_states&INPUT_BUTTON_RIGHT) {
-		actor.scr.pos.x += velocity * 1;
-		nextf = walkright;
-		arrsz = ARRSZ(walkright);
+
+	if (is_attacking) {
+		arrsz = ARRSZ(attack_right);
+		
+		if (frame_idx + 1 == arrsz)
+			is_attacking = 0;
+
+		nextf = attack_right;
+		
+	} else {
+
+		if (input_buttons_states&INPUT_BUTTON_SHOOT) {
+			is_attacking = 1;
+			frame_idx = -1;
+		} else if (input_buttons_states&INPUT_BUTTON_UP) {
+			actor.scr.pos.y -= velocity * 1;
+			nextf = walk_up;
+			arrsz = ARRSZ(walk_up);
+		} else if (input_buttons_states&INPUT_BUTTON_DOWN) {
+			actor.scr.pos.y += velocity * 1;
+			nextf = walk_down;
+			arrsz = ARRSZ(walk_down);
+		} else if (input_buttons_states&INPUT_BUTTON_LEFT) {
+			actor.scr.pos.x -= velocity * 1;
+			nextf = walk_left;
+			arrsz = ARRSZ(walk_left);
+		} else if (input_buttons_states&INPUT_BUTTON_RIGHT) {
+			actor.scr.pos.x += velocity * 1;
+			nextf = walk_right;
+			arrsz = ARRSZ(walk_right);
+		}
+
 	}
 
 	if (nextf != NULL) {
