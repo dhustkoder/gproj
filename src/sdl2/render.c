a75d19f4b49da6e4aca798b3cb668c50388bcd01
Rafael Moura <rafaelmoura.dev@gmail.com>
2019-09-26 23:38:27 -0300

    testing render thread

--- src/sdl2/render.c
+++ src/sdl2/render.c
@@ -6,11 +6,32 @@
 #include <SDL2/SDL_image.h>
 #include <SDL_FontCache.h>
 #include <tmx.h>
-#include "render.h"
+#include "threads.h"
 #include "logger.h"
 #include "map.h"
+#include "render.h"
 
 
+static volatile struct render_actors_pack {
+	const struct recti* ss_srcs;
+	const struct rectf* scr_dsts;
+    const actor_anim_flag_t* flags;
+    int count;
+} actors_pack = { NULL, NULL, NULL, 0 };
+
+static volatile struct render_text_pack {
+	char buffer[256];
+} text_packs[256];
+
+static volatile struct render_map_pack {
+	const int32_t* gids;
+    const struct vec2i* map_size;
+    const struct vec2i* tile_size;
+} map_pack;
+
+
+static volatile int text_pack_cnt = 0;
+static volatile int text_pack_draw_cnt = 0;
 
 static SDL_Window* win = NULL;
 static SDL_Renderer* rend;
@@ -25,23 +46,16 @@ static SDL_Texture* tex_prof;
 #endif	
 static FC_Font* font;
 
-const render_layer_flags_t targets_flags[RENDER_LAYER_NLAYERS] = {
-	RENDER_LAYER_BG,
-	RENDER_LAYER_ACTORS,
-	RENDER_LAYER_FG,
-	RENDER_LAYER_TXT
-	#ifdef GPROJ_PROFILING
-	,RENDER_LAYER_PROF
-	#endif
-};
-
-static SDL_Texture* targets_arr[RENDER_LAYER_NLAYERS];
-static render_layer_flags_t layers_cleared;
-
 static struct vec2i ts_size = { 0, 0 };
 static struct vec2i fb_size = { 0, 0 };
 static struct vec2i text_pos = { 0, 0 };
 static struct vec2i cam_pos = { 0, 0 };
+static volatile bool render_thr_kill = false;
+static volatile bool render_thr_terminated = false;
+static volatile bool setup_fb_needed = false;
+static volatile bool present_needed = false;
+static gproj_thread_t* render_thr; 
+
 
 
 static void draw_flipped_gid(const int32_t gid,
@@ -112,12 +126,15 @@ static void draw_map_layer(const int32_t* const gids,
 }
 
 
-void render_init(const char* const identifier)
+
+
+int render_thr_main(void* p)
 {
 	int err;
 	((void)err);
+	((void)p);
 
-	win = SDL_CreateWindow(identifier,
+	win = SDL_CreateWindow("FUCK",
 	                       SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 	                       GPROJ_SCR_WIDTH, GPROJ_SCR_HEIGHT,
 	                       SDL_WINDOW_RESIZABLE);
@@ -148,45 +165,12 @@ void render_init(const char* const identifier)
 	err = SDL_SetTextureBlendMode(tex_txt, 0);
 	assert(err == 0);
 
-
-	targets_arr[3] = tex_txt;
-	#ifdef GPROJ_PROFILING
-	targets_arr[4] = tex_prof;
-	#endif
-
-	render_clear(RENDER_LAYER_TXT);
-
-	render_present();
-}
-
-
-void render_term()
-{
-	FC_FreeFont(font);
-
-	if (tex_ts != NULL)
-		SDL_DestroyTexture(tex_ts);
-	if (tex_ss != NULL)
-		SDL_DestroyTexture(tex_ss);
-	if (tex_fg != NULL)
-		SDL_DestroyTexture(tex_fg);
-	if (tex_actors != NULL)
-		SDL_DestroyTexture(tex_actors);
-	if (tex_bg != NULL)
-		SDL_DestroyTexture(tex_bg);
-	if (tex_txt != NULL)
-		SDL_DestroyTexture(tex_txt);
-	if (rend != NULL)
-		SDL_DestroyRenderer(rend);
-	if (win != NULL)
-		SDL_DestroyWindow(win);
-}
-
-
-
-void render_fb_setup(const struct vec2i* const size)
-{
-	fb_size = *size;
+Lsetup_fb:
+	while (!setup_fb_needed) {
+		if (render_thr_kill)
+			goto Lterminate_thr;
+		timer_sleep(1);
+	}
 
 	/*
 	TODO: screen size / fb size system
@@ -214,20 +198,170 @@ void render_fb_setup(const struct vec2i* const size)
 	                           fb_size.x, fb_size.y);
 	assert(tex_fg != NULL);
 
-	int err;
-	((void)err);
 	err = SDL_SetTextureBlendMode(tex_actors, SDL_BLENDMODE_BLEND);
 	assert(err == 0);
 	err = SDL_SetTextureBlendMode(tex_fg, SDL_BLENDMODE_BLEND);
 	assert(err == 0);
 
-	targets_arr[0] = tex_bg;
-	targets_arr[1] = tex_actors;
-	targets_arr[2] = tex_fg;
+	setup_fb_needed = false;
 
-	render_clear(RENDER_LAYER_ALL);
+	
+	for (;;) {
+		if (render_thr_kill)
+			goto Lterminate_thr;
+		if (setup_fb_needed)
+			goto Lsetup_fb;
 
-	render_present();
+
+		if (present_needed) {
+			const SDL_Rect cam_rect = {
+				.x = cam_pos.x,
+				.y = cam_pos.y,
+				.w = GPROJ_SCR_WIDTH,
+				.h = GPROJ_SCR_HEIGHT
+			};
+			
+			const SDL_Rect text_rect = {
+				.x = 0,
+				.y = 0,
+				.w = text_pos.x,
+				.h = text_pos.y
+			};
+
+			SDL_SetRenderTarget(rend, NULL);
+			SDL_RenderCopy(rend, tex_bg, &cam_rect, NULL);
+			SDL_RenderCopy(rend, tex_actors, &cam_rect, NULL);
+			SDL_RenderCopy(rend, tex_fg, &cam_rect, NULL);
+			SDL_RenderCopy(rend, tex_txt, &text_rect, &text_rect);
+			SDL_RenderPresent(rend);
+
+			SDL_SetRenderTarget(rend, tex_txt);
+			SDL_RenderClear(rend);
+
+
+			text_pos.x = 0;
+			text_pos.y = 0;
+			text_pack_cnt = 0;
+			text_pack_draw_cnt = 0;
+			present_needed = false;
+			continue;
+		}
+
+
+		if (map_pack.gids != NULL) {
+			SDL_SetRenderTarget(rend, tex_bg);
+			draw_map_layer(map_pack.gids, *map_pack.map_size, *map_pack.tile_size);
+			SDL_SetRenderTarget(rend, tex_fg);
+			draw_map_layer(map_pack.gids + MAP_LAYER_FG *
+			               map_pack.map_size->x * map_pack.map_size->y,
+			               *map_pack.map_size, *map_pack.tile_size);
+			map_pack.gids = NULL;
+		}
+
+		if (actors_pack.count > 0) {
+			const struct recti* const ss_srcs = actors_pack.ss_srcs;
+			const struct rectf* const scr_dsts = actors_pack.scr_dsts;
+			const actor_anim_flag_t* const flags  = actors_pack.flags;
+			const int count = actors_pack.count;
+
+			SDL_SetRenderTarget(rend, tex_actors);
+
+			SDL_Rect scr, ss;
+			for (int i = 0; i < count; ++i) {
+				scr = (SDL_Rect) {
+					.x = scr_dsts[i].pos.x,
+					.y = scr_dsts[i].pos.y,
+					.w = scr_dsts[i].size.x,
+					.h = scr_dsts[i].size.y
+				};
+				ss = (SDL_Rect) {
+					.x = ss_srcs[i].pos.x,
+					.y = ss_srcs[i].pos.y,
+					.w = ss_srcs[i].size.x,
+					.h = ss_srcs[i].size.y
+				};
+				const int flip = flags[i]&ANIM_FLAG_FLIPH ? SDL_FLIP_HORIZONTAL : 0;
+				SDL_RenderCopyEx(rend, tex_ss, &ss, &scr, 0, NULL, flip);
+			}
+
+			actors_pack.count = 0;
+		}
+
+
+		//const int txt_cnt = text_pack_cnt;
+		//if (txt_cnt > text_pack_draw_cnt) {
+			//SDL_SetRenderTarget(rend, tex_txt);
+
+			//for (int i = text_pack_draw_cnt; i < txt_cnt; ++i) {
+				//const struct render_text_pack* const pack = &text_packs[text_pack_draw_cnt++];
+				//LOG("!!%s!!", pack->buffer);
+				/*const SDL_Rect dirty = FC_Draw(font, rend, 0, text_pos.y, pack->buffer);
+
+				if (dirty.w > text_pos.x)
+					text_pos.x = dirty.w;
+
+				text_pos.y += dirty.h;*/
+			//}
+		//}
+
+
+
+	}
+
+
+
+Lterminate_thr:
+	FC_FreeFont(font);
+
+	if (tex_ts != NULL)
+		SDL_DestroyTexture(tex_ts);
+	if (tex_ss != NULL)
+		SDL_DestroyTexture(tex_ss);
+	if (tex_fg != NULL)
+		SDL_DestroyTexture(tex_fg);
+	if (tex_actors != NULL)
+		SDL_DestroyTexture(tex_actors);
+	if (tex_bg != NULL)
+		SDL_DestroyTexture(tex_bg);
+	if (tex_txt != NULL)
+		SDL_DestroyTexture(tex_txt);
+	if (rend != NULL)
+		SDL_DestroyRenderer(rend);
+	if (win != NULL)
+		SDL_DestroyWindow(win);
+
+	render_thr_terminated = true;
+
+	return 0;
+}
+
+
+
+
+void render_init(const char* const identifier)
+{
+	render_thr = thread_start(render_thr_main, "RENDER_THREAD", NULL);
+	thread_detach(render_thr);
+}
+
+
+void render_term()
+{
+	render_thr_kill = true;
+
+	while (!render_thr_terminated)
+		timer_sleep(1);
+}
+
+
+
+void render_fb_setup(const struct vec2i* const size)
+{
+	fb_size = *size;
+	setup_fb_needed = true;
+	while (setup_fb_needed) {
+		timer_sleep(1);
+	}
 }
 
 
@@ -258,35 +392,15 @@ void render_load_ss(const char* const path)
 }
 
 
-void render_clear(const render_layer_flags_t layers)
-{
-	assert(layers != 0);
-	render_layer_flags_t toclear = layers;
-
-	for (size_t i = 0; i < ARRSZ(targets_flags); ++i) {
-		if ((toclear&targets_flags[i]) && !(targets_flags[i]&layers_cleared)) {
-			SDL_SetRenderTarget(rend, targets_arr[i]);
-			SDL_RenderClear(rend);
-			toclear &= ~targets_flags[i];
-			if (toclear == 0)
-				break;
-		}
-	}
-
-	layers_cleared |= layers;
-}
-
-
 void render_map(const int32_t* const gids,
                 const struct vec2i* const map_size,
-                const struct vec2i* const tile_size)
+                const struct vec2i* const tile_size) 
 {
-	render_clear(RENDER_LAYER_BG|RENDER_LAYER_FG);
-	SDL_SetRenderTarget(rend, tex_bg);
-	draw_map_layer(gids, *map_size, *tile_size);
-	SDL_SetRenderTarget(rend, tex_fg);
-	draw_map_layer(gids + MAP_LAYER_FG * map_size->x * map_size->y,
-	               *map_size, *tile_size);
+	map_pack = (struct render_map_pack) {
+		.gids = gids,
+		.map_size = map_size,
+		.tile_size = tile_size
+	};
 }
 
 
@@ -295,43 +409,29 @@ void render_actors(const struct recti* const ss_srcs,
                    const actor_anim_flag_t* const flags,
                    const int count)
 {
-	/* FIXME: clearing takes the most time for actors update */
-	render_clear(RENDER_LAYER_ACTORS);
-
-	SDL_Rect scr, ss;
-	for (int i = 0; i < count; ++i) {
-		scr = (SDL_Rect) {
-			.x = scr_dsts[i].pos.x,
-			.y = scr_dsts[i].pos.y,
-			.w = scr_dsts[i].size.x,
-			.h = scr_dsts[i].size.y
+	if (actors_pack.count == 0) {
+		actors_pack = (struct render_actors_pack) {
+			.ss_srcs = ss_srcs,
+			.scr_dsts = scr_dsts,
+			.flags = flags,
+			.count = count
 		};
-		ss = (SDL_Rect) {
-			.x = ss_srcs[i].pos.x,
-			.y = ss_srcs[i].pos.y,
-			.w = ss_srcs[i].size.x,
-			.h = ss_srcs[i].size.y
-		};
-		const int flip = flags[i]&ANIM_FLAG_FLIPH ? SDL_FLIP_HORIZONTAL : 0;
-		SDL_RenderCopyEx(rend, tex_ss, &ss, &scr, 0, NULL, flip);
 	}
 }
 
 
 void render_text(const char* const text, ...)
 {
+	if (present_needed)
+		text_pack_cnt = 0;
+		
+	assert(text_pack_cnt < 256);
 	va_list vargs;
 	va_start(vargs, text);
-
-	render_clear(RENDER_LAYER_TXT);
-	SDL_SetRenderTarget(rend, tex_txt);
-	const SDL_Rect dirty = FC_Draw_v(font, rend, 0, text_pos.y,
-	                                 text, vargs);
-	if (dirty.w > text_pos.x)
-		text_pos.x = dirty.w;
-	
-	text_pos.y += dirty.h;
-
+	struct render_text_pack* const pack = &text_packs[text_pack_cnt];
+	vsnprintf(pack->buffer, 256, text, vargs);
+	LOG("PUSHING STRING: %s", pack->buffer);
+	text_pack_cnt++;
 	va_end(vargs);
 }
 
@@ -351,38 +451,13 @@ void render_set_camera(int x, int y)
 #ifdef GPROJ_PROFILING
 void render_profiling_data()
 {
-	render_clear(RENDER_LAYER_PROF);
+	//render_clear(RENDER_LAYER_PROF);
 }
 #endif
 
 void render_present(void)
 {
-	render_text("CAMERA POS => (%d, %d)", cam_pos.x, cam_pos.y);
-
-	const SDL_Rect cam_rect = {
-		.x = cam_pos.x,
-		.y = cam_pos.y,
-		.w = GPROJ_SCR_WIDTH,
-		.h = GPROJ_SCR_HEIGHT
-	};
-	const SDL_Rect text_rect = {
-		.x = 0,
-		.y = 0,
-		.w = text_pos.x,
-		.h = text_pos.y
-	};	
-
-	SDL_SetRenderTarget(rend, NULL);
-	SDL_RenderCopy(rend, tex_bg, &cam_rect, NULL);
-	SDL_RenderCopy(rend, tex_actors, &cam_rect, NULL);
-	SDL_RenderCopy(rend, tex_fg, &cam_rect, NULL);
-	SDL_RenderCopy(rend, tex_txt, &text_rect, &text_rect);
-	SDL_RenderPresent(rend);
-
-	text_pos.x = 0;
-	text_pos.y = 0;
-
-	layers_cleared = 0;
+	present_needed = true;
 }
 
 
--- src/sdl2/threads.h
+++ src/sdl2/threads.h
@@ -5,12 +5,17 @@ typedef SDL_Thread gproj_thread_t;
 typedef int(*gproj_thread_fn)(void* args);
 
 
-static inline gproj_thread_t* thread_start(gproj_thread_fn thr_main, void* args)
+static inline gproj_thread_t* thread_start(gproj_thread_fn thr_main, const char* const name, void* args)
 {
-	SDL_Thread* thr = SDL_CreateThread(thr_main, "GprojThread", args);
+	SDL_Thread* const thr = SDL_CreateThread(thr_main, name, args);
 	return thr;
 }
 
+
+static inline void thread_detach(gproj_thread_t* const thr)
+{
+	SDL_DetachThread(thr);
+}
 
 static inline int thread_join(gproj_thread_t* thr)
 {
